c6ac4a07fb4f366286dff993ad917772755a35c2
dszammer <toxjr01@gmail.com>
2023-12-15 13:16:34 +0100

    start

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_lines("./part1.txt");
+
+    println!("Part 1: {}", part1(&input));
+
+    println!("Part 2: {}", part2(&input));
+}
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap() // panic on possible file-reading errors
+        .lines() // split the string into an iterator of string slices
+        .map(String::from) // make each slice into a string
+        .collect() // gather them together into a vector
+}
+
+fn part1(input: &Vec<String>) -> u32 {
+    let mut sum = 0;
+
+    sum
+}
+
+fn part2(input: &Vec<String>) -> u32 {
+    let mut sum = 0;
+
+    sum
 }
