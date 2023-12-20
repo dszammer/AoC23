78a646bf0fe5aa7f54f0907c2deee8d0532222c5
dszammer <toxjr01@gmail.com>
2023-12-20 10:33:22 +0100

    common, finally

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,17 @@
+use common;
+
 fn main() {
-    println!("Hello, world!");
+    let input = common::read_lines("./input.txt");
+
+    println!("Part 1: {}", part1(&input));
+
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &[String]) -> u32 {
+    0
+}
+
+fn part2(input: &[String]) -> u32 {
+    0
 }
