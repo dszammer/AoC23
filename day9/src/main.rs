5b1ac3d0d8a944ad69bb6ca41f14056e46c785e0
dszammer <toxjr01@gmail.com>
2024-07-17 08:08:11 +0200

    day9 p1 p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,11 +6,49 @@ fn main() {
 }
 
 fn part1(mut input: Vec<String>) -> u64 {
-    0
+    let mut result: i64 = 0;
+
+    input.iter().for_each(|line| {
+        let numbers = line
+            .split_whitespace()
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+        for (i, number) in numbers.iter().enumerate() {
+            let mut line_result = 0;
+            result += number
+                * comb(numbers.len() as i64, i as i64)
+                * (-1 as i32).pow(numbers.len() as u32 - 1 - i as u32) as i64;
+        }
+    });
+
+    result as u64
 }
 
 fn part2(mut input: Vec<String>) -> u64 {
-    0
+        let mut result: i64 = 0;
+
+    input.iter().for_each(|line| {
+        let numbers = line
+            .split_whitespace()
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+        for (i, number) in numbers.iter().enumerate() {
+            let mut line_result = 0;
+            result += number
+                * comb(numbers.len() as i64, i as i64 + 1)
+                * (-1 as i32).pow(i as u32) as i64;
+        }
+    });
+
+    result as u64
+}
+
+pub fn comb(n: i64, k: i64) -> i64 {
+    if k > n {
+        0
+    } else {
+        (1..=k).fold(1, |acc, val| acc * (n - val + 1) / val)
+    }
 }
 
 #[cfg(test)]
@@ -26,11 +64,22 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(super::part1(get_test_input()), 0);
+        assert_eq!(super::part1(get_test_input()), 114);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(super::part2(get_test_input()), 0);
+        assert_eq!(super::part2(get_test_input()), 2);
+    }
+
+    #[test]
+    fn test_comb() {
+        assert_eq!(super::comb(5, 2), 10);
+        assert_eq!(super::comb(5, 3), 10);
+        assert_eq!(super::comb(1, 1), 1);
+        assert_eq!(super::comb(0, 0), 1);
+        assert_eq!(super::comb(10, 1), 10);
+        assert_eq!(super::comb(10, 2), 45);
+        assert_eq!(super::comb(10, 3), 120);
     }
 }
